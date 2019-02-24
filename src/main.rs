0c7818946d027e565db3bdfafe68bdb84825652b
Nat Wilson <njwilson23@gmail.com>
2019-02-23 19:00:41 -0800

    Remove skip and fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::cmp::Ordering;
 use std::path::Path;
 
-use gdal::raster::{Dataset, Driver, RasterBand};
 use gdal::raster::dataset::GeoTransform;
 use gdal::raster::types::GdalType;
+use gdal::raster::{Dataset, Driver, RasterBand};
 
 use gdal_sys::GDALDataType;
 
@@ -45,13 +45,12 @@ struct Swath {
 }
 
 impl Swath {
-
     fn from_band(band: &RasterBand) -> Swath {
         let size = band.owning_dataset().size();
         let gt = band.owning_dataset().geo_transform().unwrap();
         let proj = band.owning_dataset().projection();
 
-        Swath{
+        Swath {
             nx: size.0 as isize,
             ny: size.1 as isize,
             gt,
@@ -64,7 +63,10 @@ impl Swath {
         let pt1 = apply_geotransform(self.gt, self.nx, 0);
         let pt2 = apply_geotransform(self.gt, self.nx, self.ny);
         let pt3 = apply_geotransform(self.gt, 0, self.ny);
-        vec![pt0, pt1, pt2, pt3].iter().map(|pt| (RealF64{v: pt.0}, RealF64{v: pt.1})).collect()
+        vec![pt0, pt1, pt2, pt3]
+            .iter()
+            .map(|pt| (RealF64 { v: pt.0 }, RealF64 { v: pt.1 }))
+            .collect()
     }
 
     fn left_extreme(&self) -> RealF64 {
@@ -107,16 +109,15 @@ fn invert_geotransform(gt: GeoTransform, pt: &Point) -> (f64, f64) {
     let y = pt.1.v;
 
     if b != 0.0 {
-        let ny = (b*y - b*d - e*x + e*a) / (b*f - e*c);
-        let nx = (x - a - ny*c) / b;
+        let ny = (b * y - b * d - e * x + e * a) / (b * f - e * c);
+        let nx = (x - a - ny * c) / b;
         (nx, ny)
     } else {
-        let ny = (e*x - e*a - b*y + b*d) / (e*c - b*f);
-        let nx = (y - d - ny*f) / e;
+        let ny = (e * x - e * a - b * y + b * d) / (e * c - b * f);
+        let nx = (y - d - ny * f) / e;
         (nx, ny)
     }
 }
-
 
 // Pixel size needed to include a point given a geo_transform
 fn imsize(gt: GeoTransform, pt: &Point) -> (isize, isize) {
@@ -128,9 +129,10 @@ fn imsize(gt: GeoTransform, pt: &Point) -> (isize, isize) {
 // Return the rectangular swatch representing the intersection of a sequence of
 // RasterBands. The orientation will be according to the first RasterBand.
 fn intersection(bands: &[&RasterBand]) -> Result<Swath, GeoError> {
-
     if bands.len() == 0 {
-        return Err(GeoError{msg: "No bands provided".to_string()})
+        return Err(GeoError {
+            msg: "No bands provided".to_string(),
+        });
     }
 
     let swaths: Vec<Swath> = bands.iter().map(|b| Swath::from_band(b)).collect();
@@ -146,21 +148,30 @@ fn intersection(bands: &[&RasterBand]) -> Result<Swath, GeoError> {
     let lower_top = top.iter().max().unwrap();
 
     if (rightmost_left > leftmost_right) || (upper_bottom > lower_top) {
-        Err(
-            GeoError{msg: "No valid intersection between bands".to_string()}
-        )
+        Err(GeoError {
+            msg: "No valid intersection between bands".to_string(),
+        })
     } else {
         let gt_fst = bands[0].owning_dataset().geo_transform().unwrap();
         let proj_fst = bands[0].owning_dataset().projection();
 
-        let gt: [f64; 6] = [rightmost_left.v, gt_fst[1], gt_fst[2],
-                            lower_top.v, gt_fst[4], gt_fst[5]];
+        let gt: [f64; 6] = [
+            rightmost_left.v,
+            gt_fst[1],
+            gt_fst[2],
+            lower_top.v,
+            gt_fst[4],
+            gt_fst[5],
+        ];
 
         let (nx, ny) = imsize(gt, &(*leftmost_right, *upper_bottom));
 
-        Ok(
-            Swath{nx, ny, gt, proj: proj_fst}
-        )
+        Ok(Swath {
+            nx,
+            ny,
+            gt,
+            proj: proj_fst,
+        })
     }
 }
 
@@ -171,30 +182,48 @@ fn apply_geotransform(gt: GeoTransform, xpx: isize, ypx: isize) -> (f64, f64) {
 }
 
 fn ply_bands<T: Copy + GdalType>(
-            output: &OutputOptions,
-            swath: Swath,
-            projection: String,
-            band1: &RasterBand,
-            band2: &RasterBand,
-            band3: &RasterBand) -> Result<Dataset, GeoError> {
+    output: &OutputOptions,
+    swath: Swath,
+    projection: String,
+    band1: &RasterBand,
+    band2: &RasterBand,
+    band3: &RasterBand,
+) -> Result<Dataset, GeoError> {
     let driver = Driver::get(&output.format).unwrap();
 
-    let ds = driver.create_with_band_type::<T>(&output.filename,
-                                               swath.nx.abs(),
-                                               swath.ny.abs(),
-                                               3).expect("failed to create output dataset");
+    let ds = driver
+        .create_with_band_type::<T>(&output.filename, swath.nx.abs(), swath.ny.abs(), 3)
+        .expect("failed to create output dataset");
 
     ds.set_projection(&projection).unwrap();
     ds.set_geo_transform(&swath.gt).unwrap();
 
     let buf = band1.read_band_as::<T>().unwrap();
-    ds.write_raster(1, (0, 0), (swath.nx.abs() as usize, swath.ny.abs() as usize), &buf).unwrap();
+    ds.write_raster(
+        1,
+        (0, 0),
+        (swath.nx.abs() as usize, swath.ny.abs() as usize),
+        &buf,
+    )
+    .unwrap();
 
     let buf = band2.read_band_as::<T>().unwrap();
-    ds.write_raster(2, (0, 0), (swath.nx.abs() as usize, swath.ny.abs() as usize), &buf).unwrap();
+    ds.write_raster(
+        2,
+        (0, 0),
+        (swath.nx.abs() as usize, swath.ny.abs() as usize),
+        &buf,
+    )
+    .unwrap();
 
     let buf = band3.read_band_as::<T>().unwrap();
-    ds.write_raster(3, (0, 0), (swath.nx.abs() as usize, swath.ny.abs() as usize), &buf).unwrap();
+    ds.write_raster(
+        3,
+        (0, 0),
+        (swath.nx.abs() as usize, swath.ny.abs() as usize),
+        &buf,
+    )
+    .unwrap();
 
     Ok(ds)
 }
@@ -204,15 +233,16 @@ where
     I: Iterator<Item = T>,
     T: Eq,
 {
-
-    let (result, _) = things.skip(1).fold(
-        (true, None),
-        |(b, left_opt), right| if b {
-            (left_opt.is_none() || left_opt.unwrap() == right, Some(right))
+    let (result, _) = things.fold((true, None), |(b, left_opt), right| {
+        if b {
+            (
+                left_opt.is_none() || left_opt.unwrap() == right,
+                Some(right),
+            )
         } else {
             (b, Some(right))
         }
-    );
+    });
 
     result
 }
@@ -227,15 +257,16 @@ fn have_same_projection(datasets: &[&Dataset]) -> Result<(), String> {
 }
 
 fn gt_compatible(gt1: GeoTransform, gt2: GeoTransform) -> Result<(), String> {
-    let spacing_check = if gt1[1] == gt2[1] && gt1[2] == gt2[2] && gt1[4] == gt2[4] && gt1[5] == gt2[5] {
-        Ok(())
-    } else {
-        return Err("non-equal spacing".to_string());
-    };
+    let spacing_check =
+        if gt1[1] == gt2[1] && gt1[2] == gt2[2] && gt1[4] == gt2[4] && gt1[5] == gt2[5] {
+            Ok(())
+        } else {
+            return Err("non-equal spacing".to_string());
+        };
 
     // check for integer solutions to initial offsets
     let offset_check = {
-        let origin2 = (RealF64{v: gt2[0]}, RealF64{v: gt2[3]});
+        let origin2 = (RealF64 { v: gt2[0] }, RealF64 { v: gt2[3] });
         let (nx, ny) = invert_geotransform(gt1, &origin2);
 
         if (nx % 1.0 == 0.0) && (ny % 1.0 == 0.0) {
@@ -255,54 +286,66 @@ fn have_compatible_geotransforms(datasets: &[&Dataset]) -> Result<(), String> {
 
     let (result, _) = datasets.iter().fold(
         (Ok(()), datasets[0].geo_transform().unwrap()),
-        |(acc, left), right_ds| if acc.is_ok() {
-            let right = right_ds.geo_transform().unwrap();
-            (gt_compatible(left, right), right)
-        } else {
-            (acc, left)
-        });
+        |(acc, left), right_ds| {
+            if acc.is_ok() {
+                let right = right_ds.geo_transform().unwrap();
+                (gt_compatible(left, right), right)
+            } else {
+                (acc, left)
+            }
+        },
+    );
 
     result
 }
 
 fn main() {
-
     let cli = App::new("plyband")
-                .version("0.1.0")
-                .about("Combines satellite bands into false-colour images")
-                .author("Nat Wilson")
-                .arg(Arg::with_name("red")
-                     .short("r")
-                     .long("red")
-                     .value_name("INPUT")
-                     .help("Source of red channel")
-                     .required(true)
-                     .display_order(1))
-                .arg(Arg::with_name("green")
-                     .short("g")
-                     .long("green")
-                     .value_name("INPUT")
-                     .help("Source of green channel")
-                     .required(true)
-                     .display_order(2))
-                .arg(Arg::with_name("blue")
-                     .short("b")
-                     .long("blue")
-                     .value_name("INPUT")
-                     .help("Source of blue channel")
-                     .required(true)
-                     .display_order(3))
-                .arg(Arg::with_name("output")
-                     .short("o")
-                     .long("output")
-                     .value_name("INPUT")
-                     .help("Output file")
-                     .required(false))
-                .arg(Arg::with_name("output_format")
-                     .long("output-format")
-                     .value_name("FORMAT")
-                     .help("Output format driver"))
-                .get_matches();
+        .version("0.1.0")
+        .about("Combines satellite bands into false-colour images")
+        .author("Nat Wilson")
+        .arg(
+            Arg::with_name("red")
+                .short("r")
+                .long("red")
+                .value_name("INPUT")
+                .help("Source of red channel")
+                .required(true)
+                .display_order(1),
+        )
+        .arg(
+            Arg::with_name("green")
+                .short("g")
+                .long("green")
+                .value_name("INPUT")
+                .help("Source of green channel")
+                .required(true)
+                .display_order(2),
+        )
+        .arg(
+            Arg::with_name("blue")
+                .short("b")
+                .long("blue")
+                .value_name("INPUT")
+                .help("Source of blue channel")
+                .required(true)
+                .display_order(3),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("INPUT")
+                .help("Output file")
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("output_format")
+                .long("output-format")
+                .value_name("FORMAT")
+                .help("Output format driver"),
+        )
+        .get_matches();
 
     let red_input = cli.value_of("red").unwrap();
     let green_input = cli.value_of("green").unwrap();
@@ -329,7 +372,7 @@ fn main() {
 
     let swath = intersection(&[&red_band, &green_band, &blue_band]).unwrap();
 
-    let output_options = OutputOptions{
+    let output_options = OutputOptions {
         filename: output.to_string(),
         format: output_format.to_string(),
     };
@@ -337,25 +380,68 @@ fn main() {
     let pixel_type = red_band.band_type();
 
     let result = match pixel_type {
-        GDALDataType::GDT_Byte =>
-            ply_bands::<u8>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        GDALDataType::GDT_UInt16 =>
-            ply_bands::<u16>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        GDALDataType::GDT_UInt32 =>
-            ply_bands::<u32>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        GDALDataType::GDT_Int16 =>
-            ply_bands::<i16>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        GDALDataType::GDT_Int32 =>
-            ply_bands::<i32>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        GDALDataType::GDT_Float32 =>
-            ply_bands::<f32>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        GDALDataType::GDT_Float64 =>
-            ply_bands::<f64>(&output_options, swath, proj, &red_band, &green_band, &blue_band),
-        _ => Err(GeoError{msg: format!("Unhandled band type {}", pixel_type).to_string()}),
+        GDALDataType::GDT_Byte => ply_bands::<u8>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        GDALDataType::GDT_UInt16 => ply_bands::<u16>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        GDALDataType::GDT_UInt32 => ply_bands::<u32>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        GDALDataType::GDT_Int16 => ply_bands::<i16>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        GDALDataType::GDT_Int32 => ply_bands::<i32>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        GDALDataType::GDT_Float32 => ply_bands::<f32>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        GDALDataType::GDT_Float64 => ply_bands::<f64>(
+            &output_options,
+            swath,
+            proj,
+            &red_band,
+            &green_band,
+            &blue_band,
+        ),
+        _ => Err(GeoError {
+            msg: format!("Unhandled band type {}", pixel_type).to_string(),
+        }),
     };
 
     result.unwrap();
-
 
     // TODO:
     // - windowed read/write
